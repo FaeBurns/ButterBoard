ece96bed209b9f2ba704d9b670cdf5a2bc1a4595
Fae Burns <fyredemon405@gmail.com>
2023-05-04 18:43:25 +0100

    Updated background image

---
+++ Assets/Scripts/BackgroundPositioner.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+
+namespace ButterBoard
+{
+    public class BackgroundPositioner : MonoBehaviour
+    {
+        private Camera _camera = null!;
+
+        [SerializeField]
+        private SpriteRenderer backgroundSpriteRenderer = null!;
+
+        [SerializeField]
+        private int moveThreshold = 5;
+
+        [SerializeField]
+        private float tileSizeScale = 0.25f;
+
+        private void Awake()
+        {
+            _camera = Camera.main!;
+            OnValidate();
+        }
+
+        private void OnValidate()
+        {
+            transform.localScale = new Vector3(moveThreshold, moveThreshold, 1);
+        }
+
+        private void LateUpdate()
+        {
+            Vector2 cameraPosition = _camera.transform.position;
+            Vector2 flooredCameraPosition = new Vector2(Mathf.Floor(cameraPosition.x / moveThreshold), Mathf.Floor(cameraPosition.y / moveThreshold));
+            Vector2 scaledBackPosition = flooredCameraPosition * moveThreshold;
+            transform.position = new Vector3(scaledBackPosition.x, scaledBackPosition.y, 0);
+
+            UpdateTilingSize();
+        }
+
+        private void UpdateTilingSize()
+        {
+            Vector2 viewSize = GetCameraViewSize();
+
+            // scale viewSize by size of individual tile
+            Vector2 scaledSize = viewSize / moveThreshold;
+
+            // snap to 2s - looks nicer
+            Vector2 finalSnappedSize = SnapVectorCeil(scaledSize, 2);
+
+            // add 2 to account for camera offset
+            finalSnappedSize += new Vector2(2, 2);
+            backgroundSpriteRenderer.size = finalSnappedSize;
+        }
+
+        private Vector2 GetCameraViewSize()
+        {
+            float cameraHeight = _camera.orthographicSize * 2;
+            return new Vector2(cameraHeight * _camera.aspect, cameraHeight);
+        }
+
+        private Vector2 SnapVectorFloor(Vector2 vector, float snap)
+        {
+            return new Vector2(Mathf.Floor(vector.x / snap) * snap, Mathf.Floor(vector.y / snap) * snap);
+        }
+
+        private Vector2 SnapVectorCeil(Vector2 vector, float snap)
+        {
+            return new Vector2(Mathf.Ceil(vector.x / snap) * snap, Mathf.Ceil(vector.y / snap) * snap);
+        }
+    }
+}
